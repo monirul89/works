2d54def5ed2a49673f5cc4d5ae99b5e752e1d014
monirul89 <monirul89@gmail.com>
2019-02-27 06:52:35 +0600

    no message

---
+++ theshoptd/mail.php
@@ -0,0 +1,34 @@
+<?php
+//if "email" variable is filled out, send email
+  if (isset($_REQUEST['email']))  {
+  
+  //Email information
+  $admin_email = "codemen.islam@gmail.com";
+  $email = $_REQUEST['email'];
+  $subject = $_REQUEST['subject'];
+  $comment = $_REQUEST['comment'];
+  
+  //send email
+  mail($admin_email, "$subject", $comment, "From:" . $email);
+  
+  //Email response
+  echo "Thank you for contacting us!";
+  }
+  
+  //if "email" variable is not filled out, display the form
+ 
+?>
+
+ <form method="post">
+
+  Email: <input name="email" type="text" />
+
+  Subject: <input name="subject" type="text" />
+
+  Message:
+
+  <textarea name="comment" rows="15" cols="40"></textarea>
+
+  <input type="submit" value="Submit" />
+  </form>
+  
