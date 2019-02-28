047fb508f02a6424a3f286967db101a2d7341182
monirul89 <monirul89@gmail.com>
2019-02-28 17:45:56 +0600

    no message

--- theshoptd/theshoptd/header.php
+++ theshoptd/theshoptd/header.php
@@ -16,7 +16,7 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <title>The Shoptd ||</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="keywords" content="Grocery Shoppy Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
               Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
@@ -37,7 +37,7 @@
         <link href="<?php echo get_template_directory_uri(); ?>/assets/css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
         <!--//pop-up-box-->
         <!-- price range -->
-        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/jquery-ui1.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/jquery-ui1.css">
         <!-- fonts -->
         <?php wp_head(); ?>
     </head>
@@ -54,7 +54,7 @@
                 <!-- header-bot-->
                 <div class="col-md-4 logo_agile">
                     <h1>
-                        <a href="index.html">
+                        <a href="<?php echo get_template_directory_uri(); ?>">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo3.png" alt=" ">
                         </a>
                     </h1>
@@ -573,28 +573,11 @@
         </div>
         <!-- //Modal2 -->
         <!-- //signup Model -->
+        
         <!-- //header-bot -->
         <!-- navigation -->
-        <div class="ban-top">
-            <div class="container">
-                <div class="agileits-navi_search">
-                    <form action="#" method="post">
-                        <select id="agileinfo-nav_search" name="agileinfo_search" required="">
-                            <option value="">All Categories</option>
-                            <option value="Kitchen">Kitchen</option>
-                            <option value="Household">Household</option>
-                            <option value="Snacks &amp; Beverages">Snacks & Beverages</option>
-                            <option value="Personal Care">Personal Care</option>
-                            <option value="Gift Hampers">Gift Hampers</option>
-                            <option value="Fruits &amp; Vegetables">Fruits & Vegetables</option>
-                            <option value="Baby Care">Baby Care</option>
-                            <option value="Soft Drinks &amp; Juices">Soft Drinks & Juices</option>
-                            <option value="Frozen Food">Frozen Food</option>
-                            <option value="Bread &amp; Bakery">Bread & Bakery</option>
-                            <option value="Sweets">Sweets</option>
-                        </select>
-                    </form>
-                </div>
+        <div class="">
+            <div class="container ban-top">
                 <div class="top_nav_left">
                     <nav class="navbar navbar-default">
                         <div class="container-fluid">
@@ -610,158 +593,11 @@
                             </div>
                             <!-- Collect the nav links, forms, and other content for toggling -->
                             <div class="collapse navbar-collapse menu--shylock" id="bs-example-navbar-collapse-1">
-                                <ul class="nav navbar-nav menu__list">
-                                    <li class="active">
-                                        <a class="nav-stylehead" href="index.html">Home
-                                            <span class="sr-only">(current)</span>
-                                        </a>
-                                    </li>
-                                    <li class="">
-                                        <a class="nav-stylehead" href="about.html">About Us</a>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a href="#" class="dropdown-toggle nav-stylehead" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Kitchen
-                                            <span class="caret"></span>
-                                        </a>
-                                        <ul class="dropdown-menu multi-column columns-3">
-                                            <div class="agile_inner_drop_nav_info">
-                                                <div class="col-sm-4 multi-gd-img">
-                                                    <ul class="multi-column-dropdown">
-                                                        <li>
-                                                            <a href="product.html">Bakery</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Baking Supplies</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Coffee, Tea & Beverages</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Dried Fruits, Nuts</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Sweets, Chocolate</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Spices & Masalas</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Jams, Honey & Spreads</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                                <div class="col-sm-4 multi-gd-img">
-                                                    <ul class="multi-column-dropdown">
-                                                        <li>
-                                                            <a href="product.html">Pickles</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Pasta & Noodles</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Rice, Flour & Pulses</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Sauces & Cooking Pastes</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Snack Foods</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Oils, Vinegars</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product.html">Meat, Poultry & Seafood</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                                <div class="col-sm-4 multi-gd-img">
-                                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/nav.png" alt="">
-                                                </div>
-                                                <div class="clearfix"></div>
-                                            </div>
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a href="#" class="dropdown-toggle nav-stylehead" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Household
-                                            <span class="caret"></span>
-                                        </a>
-                                        <ul class="dropdown-menu multi-column columns-3">
-                                            <div class="agile_inner_drop_nav_info">
-                                                <div class="col-sm-6 multi-gd-img">
-                                                    <ul class="multi-column-dropdown">
-                                                        <li>
-                                                            <a href="product2.html">Kitchen & Dining</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Detergents</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Utensil Cleaners</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Floor & Other Cleaners</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Disposables, Garbage Bag</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Repellents & Fresheners</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html"> Dishwash</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                                <div class="col-sm-6 multi-gd-img">
-                                                    <ul class="multi-column-dropdown">
-                                                        <li>
-                                                            <a href="product2.html">Pet Care</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Cleaning Accessories</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Pooja Needs</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Crackers</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Festive Decoratives</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Plasticware</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="product2.html">Home Care</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                                <div class="clearfix"></div>
-                                            </div>
-                                        </ul>
-                                    </li>
-                                    <li class="">
-                                        <a class="nav-stylehead" href="faqs.html">Faqs</a>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a class="nav-stylehead dropdown-toggle" href="#" data-toggle="dropdown">Pages
-                                            <b class="caret"></b>
-                                        </a>
-                                        <ul class="dropdown-menu agile_short_dropdown">
-                                            <li>
-                                                <a href="icons.html">Web Icons</a>
-                                            </li>
-                                            <li>
-                                                <a href="typography.html">Typography</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li class="">
-                                        <a class="nav-stylehead" href="contact.html">Contact</a>
-                                    </li>
-                                </ul>
+                                <?php wp_nav_menu( array(
+                                        'theme_location' => 'top',
+                                        'menu_id'        => 'top-menu',
+                                        'menu_class'    =>'nav navbar-nav menu__list',
+                                ) ); ?>
                             </div>
                         </div>
                     </nav>
