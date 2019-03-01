c08f76f9a8ca06d122b89cbb8757a948426cb6fd
monirul89 <monirul89@gmail.com>
2019-03-02 01:11:42 +0600

    no message

--- theshoptd/theshoptd/index.php
+++ theshoptd/theshoptd/index.php
@@ -17,52 +17,69 @@
 get_header();
 ?>
 
-<div class="wrap">
-    <?php if (is_home() && !is_front_page()) : ?>
-        <header class="page-header">
-            <h1 class="page-title"><?php single_post_title(); ?></h1>
-        </header>
-    <?php else : ?>
-        <header class="page-header">
-            <h2 class="page-title"><?php _e('Posts', 'theshoptd'); ?></h2>
-        </header>
-    <?php endif; ?>
+<!-- banner-2 -->
+<div class="page-head_agile_info_w3l">
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+</div>
+<!-- //banner-2 -->
+<!-- page -->
+<div class="services-breadcrumb">
+    <div class="agile_inner_breadcrumb">
+        <div class="container">
+            <ul class="w3_short">
+                <li>
+                    <a href="index.html">Home</a>
+                    <i>|</i>
+                </li>
+                <li>Faqs</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- //page -->
+<!-- FAQ-help-page -->
+<div class="faqs-w3l">
+    <div class="container">
+        <?php if (is_home() && !is_front_page()) : ?>
+            <header class="page-header">
+                <h1 class="page-title"><?php single_post_title(); ?></h1>
+            </header>
+        <?php else : ?>
+            <header class="page-header">
+                <h2 class="page-title"><?php _e('Posts', 'theshoptd'); ?></h2>
+            </header>
+        <?php endif; ?>
 
-            <?php
-            if (have_posts()) :
+        <?php
+        if (have_posts()) :
 
-                /* Start the Loop */
-                while (have_posts()) :
-                    the_post();
+            /* Start the Loop */
+            while (have_posts()) :
+                the_post();
 
-                    /*
-                     * Include the Post-Format-specific template for the content.
-                     * If you want to override this in a child theme, then include a file
-                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                     */
-                    get_template_part('template-parts/post/content', get_post_format());
+                /*
+                 * Include the Post-Format-specific template for the content.
+                 * If you want to override this in a child theme, then include a file
+                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                 */
+                get_template_part('template-parts/post/content', get_post_format());
 
-                endwhile;
+            endwhile;
 
-                the_posts_pagination(
-                        array(
-                            'prev_text' => theshoptd_get_svg(array('icon' => 'arrow-left')) . '<span class="screen-reader-text">' . __('Previous page', 'theshoptd') . '</span>',
-                            'next_text' => '<span class="screen-reader-text">' . __('Next page', 'theshoptd') . '</span>' . theshoptd_get_svg(array('icon' => 'arrow-right')),
-                            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __('Page', 'theshoptd') . ' </span>',
-                        )
-                );
+            the_posts_pagination(
+                    array(
+                        'prev_text' => theshoptd_get_svg(array('icon' => 'arrow-left')) . '<span class="screen-reader-text">' . __('Previous page', 'theshoptd') . '</span>',
+                        'next_text' => '<span class="screen-reader-text">' . __('Next page', 'theshoptd') . '</span>' . theshoptd_get_svg(array('icon' => 'arrow-right')),
+                        'before_page_number' => '<span class="meta-nav screen-reader-text">' . __('Page', 'theshoptd') . ' </span>',
+                    )
+            );
 
-            else :
+        else :
 
-                get_template_part('template-parts/post/content', 'none');
+            get_template_part('template-parts/post/content', 'none');
 
-            endif;
-            ?>
-
-        </main><!-- #main -->
+        endif;
+        ?>
     </div><!-- #primary -->
     <?php get_sidebar(); ?>
 </div><!-- .wrap -->
--- theshoptd/theshoptd/page.php
+++ theshoptd/theshoptd/page.php
@@ -17,25 +17,42 @@
 get_header();
 ?>
 
-<div class="wrap">
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+<!-- banner-2 -->
+<div class="page-head_agile_info_w3l">
 
-            <?php
-            while (have_posts()) :
-                the_post();
+</div>
+<!-- //banner-2 -->
+<!-- page -->
+<div class="services-breadcrumb">
+    <div class="agile_inner_breadcrumb">
+        <div class="container">
+            <ul class="w3_short">
+                <li>
+                    <a href="index.html">Home</a>
+                    <i>|</i>
+                </li>
+                <li>Faqs</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- //page -->
+<!-- FAQ-help-page -->
+<div class="faqs-w3l">
+    <div class="container">
+        <?php
+        while (have_posts()) :
+            the_post();
 
-                get_template_part('template-parts/page/content', 'page');
+            get_template_part('template-parts/page/content', 'page');
 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if (comments_open() || get_comments_number()) :
-                    comments_template();
-                endif;
+            // If comments are open or we have at least one comment, load up the comment template.
+            if (comments_open() || get_comments_number()) :
+                comments_template();
+            endif;
 
-            endwhile; // End of the loop.
-            ?>
-
-        </main><!-- #main -->
+        endwhile; // End of the loop.
+        ?>
     </div><!-- #primary -->
 </div><!-- .wrap -->
 
