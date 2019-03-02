ddd51bf7e8f7f435b353863038d65c21a0f9c6fb
monirul89 <monirul89@gmail.com>
2019-03-02 08:49:40 +0600

    no message

--- theshoptd/theshoptd/template-parts/page/content-page.php
+++ theshoptd/theshoptd/template-parts/page/content-page.php
@@ -10,22 +10,23 @@
  * @version 1.0
  */
 ?>
+<div class="col-sm-12">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header">
+            <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+            <?php theshoptd_edit_link(get_the_ID()); ?>
+        </header><!-- .entry-header -->
+        <div class="entry-content">
+            <?php
+            the_content();
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-        <?php theshoptd_edit_link(get_the_ID()); ?>
-    </header><!-- .entry-header -->
-    <div class="entry-content">
-        <?php
-        the_content();
-
-        wp_link_pages(
-                array(
-                    'before' => '<div class="page-links">' . __('Pages:', 'theshoptd'),
-                    'after' => '</div>',
-                )
-        );
-        ?>
-    </div><!-- .entry-content -->
-</article><!-- #post-## -->
+            wp_link_pages(
+                    array(
+                        'before' => '<div class="page-links">' . __('Pages:', 'theshoptd'),
+                        'after' => '</div>',
+                    )
+            );
+            ?>
+        </div><!-- .entry-content -->
+    </article><!-- #post-## -->
+</div>
