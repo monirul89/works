ba6f3b7d0f5596d48f59e32b7cf2404b7032d0b6
monirul89 <monirul89@gmail.com>
2019-03-20 14:48:23 +0600

    no message

--- radixetcltd/front-page.php
+++ radixetcltd/front-page.php
@@ -11,53 +11,53 @@
  * @since 1.0
  * @version 1.0
  */
-
-get_header(); ?>
+get_header();
+?>
 
 <section>    
-	<div class="site-section">        
-		<div class="container">            
-			<div class="row">                
-				<div class="col-md-12 col-sm-12 col-xs-12">
+    <div class="site-section">        
+        <div class="container">            
+            <div class="row">                
+                <div class="col-md-12 col-sm-12 col-xs-12">
 
-						<?php
-						// Show the selected front page content.
-						if ( have_posts() ) :
-							while ( have_posts() ) :
-								the_post();
-								get_template_part( 'template-parts/page/content', 'front-page' );
-							endwhile;
-						else :
-							get_template_part( 'template-parts/post/content', 'none' );
-						endif;
-						?>
+                    <?php
+                    // Show the selected front page content.
+                    if (have_posts()) :
+                        while (have_posts()) :
+                            the_post();
+                            get_template_part('template-parts/page/content', 'front-page');
+                        endwhile;
+                    else :
+                        get_template_part('template-parts/post/content', 'none');
+                    endif;
+                    ?>
 
-						<?php
-						// Get each of our panels and show the post data.
-						if ( 0 !== radixetcltd_panel_count() || is_customize_preview() ) : // If we have pages to show.
+                    <?php
+                    // Get each of our panels and show the post data.
+                    if (0 !== radixetcltd_panel_count() || is_customize_preview()) : // If we have pages to show.
 
-							/**
-							 * Filter number of front page sections in Radixetcltd.
-							 *
-							 * @since Radixetcltd 1.0
-							 *
-							 * @param int $num_sections Number of front page sections.
-							 */
-							$num_sections = apply_filters( 'radixetcltd_front_page_sections', 4 );
-							global $radixetcltdcounter;
+                        /**
+                         * Filter number of front page sections in Radixetcltd.
+                         *
+                         * @since Radixetcltd 1.0
+                         *
+                         * @param int $num_sections Number of front page sections.
+                         */
+                        $num_sections = apply_filters('radixetcltd_front_page_sections', 4);
+                        global $radixetcltdcounter;
 
-							// Create a setting and control for each of the sections available in the theme.
-							for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-								$radixetcltdcounter = $i;
-								radixetcltd_front_page_section( null, $i );
-							}
+                        // Create a setting and control for each of the sections available in the theme.
+                        for ($i = 1; $i < ( 1 + $num_sections ); $i++) {
+                            $radixetcltdcounter = $i;
+                            radixetcltd_front_page_section(null, $i);
+                        }
 
-					endif; // The if ( 0 !== radixetcltd_panel_count() ) ends here.
-						?>
-				</div>
-			</div>
-		</div>
-	</div>
+                    endif; // The if ( 0 !== radixetcltd_panel_count() ) ends here.
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 <?php
 get_footer();
