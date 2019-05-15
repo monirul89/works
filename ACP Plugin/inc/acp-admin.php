b738f8f389bd21b6d41eed482bec69ef0fc78514
MD. MONIRUL ISLAM <monirul89@gmail.com>
2019-05-15 16:13:46 +0600

    no message

--- ACP Plugin/inc/acp-admin.php
+++ ACP Plugin/inc/acp-admin.php
@@ -20,10 +20,58 @@
 
  function acp_create_plugin_page(){
     ?>
+
     <h3>ACP Admin Panal </h3>
-    <div class="control-form">
-        <label>Custom Post Name : </label>
-        <input type="text" placeholder="Custom Post Name"/>
-    </div>
+    <style>
+    table, td, th {
+        border: 1px solid #b3b3ff;
+        padding:3px 10px;
+    }
+    tr{
+        clear:both;
+        overflow:hidden;
+    }
+    .acp_post_table {
+        border-collapse: collapse;
+        border-color:#b3b3ff;
+    }
+    </style>
+    <table class="acp_post_table">
+        <tr>
+            <th>Post Name</th>
+            <th>Post Slag Name</th>
+            <th>Post Shortcode</th>
+            <th>Action</th>
+        </tr>
+        <tr>
+            <td><input type="text" name="acp_post_name" class="acp_post_name" id="acp_post_name"></td>
+            <td><input type="text" name="acp_post_name" class="acp_post_name" id="acp_post_name"></td>
+            <td><input type="text" name="acp_post_name" class="acp_post_name" id="acp_post_name"></td>
+            <td><a href="#edit">Edit</a><a href="#delete">Delete</a></td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+            <form action="" method="">
+                <div class="control-form">
+                    <button>Add Post</button>
+                </div>
+            </form>
+            </td>
+        <tr>
+    </table>
+
+     <script>
+        function post_loop(){
+
+        }
+     </script>
+        
+
+
+
     <?php
  }
